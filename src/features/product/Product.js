152c34be691945a3b018d2b3b40f5ffa6a788e10
starlight <starlight201212@gmail.com>
2021-08-02 04:39:11 +0900

    create main component

--- src/features/product/Product.js
+++ src/features/product/Product.js
@@ -1,67 +1,99 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
+  fetchProductAsync,
+  selectProducts,
+  selectIsLoading,
 } from './productSlice';
-import styles from './Product.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Loading from '../../components/LoadingComponent.jsx';
+import '../../components/style.css';
 
 export function Product() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const useStyles = makeStyles({
+    root: {
+      width: '100%',
+      position: 'relative',
+      cursor: 'pointer',
+      borderRadius: '10px'
 
-  const incrementValue = Number(incrementAmount) || 0;
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  const classes = useStyles();
+
+  const products = useSelector(selectProducts);
+  const isLoading = useSelector(selectIsLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProductAsync(''));
+  }, [dispatch]);
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    <>
+      {
+        products === [] || isLoading === true ? <Loading /> :
+          <div className={'product-div'}>
+            <h2 className={'title-text'}>Latest Products</h2>
+            <Card className={classes.root}>
+              <CardContent>
+                {
+                  products.map((product) => {
+                    return (
+                      <div
+                        key={product.id}
+                        className={'parent-div'}
+                      >
+                        <div className={'sub-div'}>
+                          <a href={product.discussion_url.split('?')[0]}>
+                            <img src={product.thumbnail.image_url} className={'video-rt'} alt="product" />
+                          </a>
+                          <div className={'sub-parent-div'}>
+                            <h4>{product.name}</h4>
+                            <a className={'a-child'} href={product.discussion_url.split('?')[0]}>{product.tagline}</a>
+                            <div className={'child-div'}>
+                              <div className={'child-div-rt'}>
+                                <a className={'a-child'} href={product.discussion_url.split('?')[0]}>
+                                  <span className={'custom-span'}>{product.comments_count}</span>
+                                </a>
+                                <span className={'custom-span custom-border-rt'}>Free</span>
+                                <a className={'a-child'} href={product.redirect_url.split('/r')[0] + '/topics/' + product.topics[0].slug}>
+                                  <span className={'custom-span'}>{product.topics[0].name}</span>
+                                </a>
+                              </div>
+                            </div>
+
+                          </div>
+                        </div>
+                        <div className={'sub-div-right'}>
+                          <Button className={'custom-button'}>
+                            <span>{product.votes_count}</span>
+                          </Button>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+              </CardContent>
+            </Card>
+          </div>
+      }
+
+    </>
   );
 }
